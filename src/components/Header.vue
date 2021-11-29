21c3cd46e9eb4257f1eeb1967cf2976125e7a159
giulio30795 <giulioincarbone95@gmail.com>
2021-11-29 10:43:14 +0100

    setup

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <section>
+        <input type="text" placeholder="Search">
+        <button>Search</button>
+    </section>
+</template>
+
+<script>
+export default {
+name: 'Header'
+}
+</script>
+
+<style scoped lang="scss">
+
+section{
+    padding: 1rem;
+}
+
+button{
+    margin-left: 1rem;
+}
+</style>
---
+++ src/components/MainContent.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+      <ul>
+          <li>test</li>
+      </ul>
+  </section>
+</template>
+
+<script>
+export default {
+name: 'MainContent'
+}
+</script>
+
+<style scoped lang="scss">
+
+
+</style>
